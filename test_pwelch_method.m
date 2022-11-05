be6ce0277a25d0e9f5cb7226fc0b2f8d20615eb5
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2022-11-05 10:03:23 +0100

    ST006 implemented fft, created plots to compare to pwelch

--- test_pwelch_method.m
+++ test_pwelch_method.m
@@ -4,16 +4,38 @@ close all;clear all;
 patient_id=1;
 % here eeg, laball, artifactsall is loaded
 load(sprintf('./Training Data/DATAall_cleaneog_A0%dT_Fs250',patient_id));
-
-%Remove artifactual trials
-artifacts=find(artifactsall==1);
-eeg(:,:,artifacts)=[];
-laball(artifacts)=[];
 [channels,N,trials]=size(eeg);
 
 x=eeg(1,:,1);
-
 fs=250; %sampling rate
+
+t=tiledlayout(2,2);
+t.TileSpacing='compact';
+
+nexttile;
+% print pure amplitude signal
+time=((0:N-1)/fs)-2;
+plot(time,x);
+title('Time domain')
+xlabel('s')
+ylabel('signal [µV]')
+
+nexttile;
+% TODO: check fft2,fftn,fft2n,fftw,fftshift,ifft,fftfilt
+y=fft(x);
+f=fs*(0:(N/2))/N;
+% TODO: think about/investigate why to compute the two-sided and single
+% sided spectrum
+P2 = abs(y/N);
+P1 = P2(1:N/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+plot(f,P1) 
+title('FFT of signal data')
+xlabel('Hz')
+ylabel('amplitude [µV]')
+
+nexttile;
+% print unscaled power spectral density
 nfft=N;
 noverlap=nfft/2;
 % TODO: check different windowing methods
@@ -21,10 +43,15 @@ window=hanning(nfft);
 % window1=hamming(nfft);
 % window=rectwin(nfft);
 [pxx,f]=pwelch(x,window,noverlap,nfft,fs);
+plot(f,pxx);
+title('Welch Power Spectral density estimate')
+xlabel('Hz')
+ylabel('W/Hz')
 
+nexttile;
+% print scaled power spectral density by logarithm
 PdB_Hz= 10*log10(pxx);
-
-figure;plot(f,PdB_Hz);
+plot(f,PdB_Hz);
 title('Welch Power Spectral density estimate')
 xlabel('Hz')
 ylabel('dBW/Hz')

01b5f21e6182bd23c8b96e3a58e949b58fa234c6
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2022-11-12 12:39:02 +0100

    added reference of used function

--- cohens_kappa.m
+++ cohens_kappa.m
@@ -1,5 +1,8 @@
-% TODO: from https://de.mathworks.com/matlabcentral/fileexchange/69943-simple-cohen-s-kappa
+% Simple Cohen's kappa (version 1.0.0)
+% Implemented by Elliot Layden
 function kappa = cohens_kappa(C)
+% Matlab function computes Cohen's kappa from observed categories and predicted categories
+% https://github.com/elayden/cohensKappa
 n = sum(C(:)); % get total N
 C = C./n; % Convert confusion matrix counts to proportion of n
 r = sum(C,2); % row sum

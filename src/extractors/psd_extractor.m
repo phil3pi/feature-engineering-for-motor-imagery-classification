cc959404356570695cf0af4a6d9e037cbfe8c82d
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2022-11-16 18:40:29 +0100

    ST010 extended statistic extractor

--- src/extractors/psd_extractor.m
+++ src/extractors/psd_extractor.m
@@ -3,14 +3,14 @@ function [psd] = psd_extractor(eeg,fs)
 
 % delta theta alpha beta gamma high gamma broad
 bands=[0.1 3.5; 4 7.5; 8 12.5; 13 29.5; 30 60; 60.5 100; 0.1 100];
+features=["mean","median","std"];
 [channels,N,trials]=size(eeg);
-nfft=N;
-pxx_length=inline_if(mod(nfft,2)==0,(nfft/2+1),(nfft+1)/2);
+%nfft=N;
+%pxx_length=inline_if(mod(nfft,2)==0,(nfft/2+1),(nfft+1)/2);
 % TODO: check different windowing methods
 % window=hamming(nfft);
 % window=rectwin(nfft);
-statistic_feature_size=1;
-psd=nan(channels*statistic_feature_size,size(bands,1),trials);
+psd=nan(channels*length(features),size(bands,1),trials);
 for trial=1:trials
         x=squeeze(eeg(:,:,trial))';
         [pxx,f]=pwelch(x,[],[],[],fs);
@@ -18,7 +18,7 @@ for trial=1:trials
             [mm,ii1]=min(abs(f-bands(j,1)));
             [mm,ii2]=min(abs(f-bands(j,2)));
                     %psd(:,j,trial)=mean(pxx(ii1:ii2,:));
-                    psd(:,j,trial)=statistic_extractor_2d(pxx(ii1:ii2,:));
+                    psd(:,j,trial)=statistic_extractor_2d(pxx(ii1:ii2,:),features);
         end
 end
 
--- src/extractors/statistic_extractor_2d.m
+++ src/extractors/statistic_extractor_2d.m
@@ -1,20 +1,20 @@
-function [statistic_features] = statistic_extractor_2d(data)
+function [statistic_features] = statistic_extractor_2d(data,selected_features)
 % Extracts statistic features of dimension[data-points x channels]
 %   The statistics are created for the data-points of each channel and
 %   trial. Following statistic features are created: min, max, mean,
 %   median, standard-deviation, kurtosis, skewness, percentile, entropy
-min_data=min(data,[],1);
-max_data=max(data,[],1);
-mean_data=mean(data);
-median_data=median(data);
+min_data=inline_if(any(strcmp(selected_features,"min")),min(data,[],1),[]);
+max_data=inline_if(any(strcmp(selected_features,"max")),max(data,[],1),[]);
+mean_data=inline_if(any(strcmp(selected_features,"mean")),mean(data),[]);
+median_data=inline_if(any(strcmp(selected_features,"median")),median(data),[]);
 % TODO: checkout different weighting schemes (0 is default)
 weight_scheme=0;
-std_data=std(data,weight_scheme);
-kurtosis_data=kurtosis(data);
-skewness_data=skewness(data);
+std_data=inline_if(any(strcmp(selected_features,"std")),std(data,weight_scheme),[]);
+kurtosis_data=inline_if(any(strcmp(selected_features,"kurtosis")),kurtosis(data),[]);
+skewness_data=inline_if(any(strcmp(selected_features,"skewness")),skewness(data),[]);
 
 percentile=50;
-prctile_data=prctile(data,percentile);
+prctile_data=inline_if(any(strcmp(selected_features,"prctile")),prctile(data,percentile),[]);
 
 % [channels,~,trials]=size(data);
 % entropy_data=nan(channels,1,trials);
@@ -27,6 +27,6 @@ prctile_data=prctile(data,percentile);
 % end
 
 % TODO: add other wanted features here
-statistic_features=[mean_data];
+statistic_features=[min_data,max_data,mean_data,median_data,std_data,kurtosis_data,skewness_data,prctile_data];
 end
 

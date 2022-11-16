8b6e7fab524f163a0be8ae7c28974e308517946e
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2022-11-16 22:01:47 +0100

    ST012 created frequencyBand class, improved code structure

---
+++ src/data/FrequencyBand.m
@@ -0,0 +1,44 @@
+classdef FrequencyBand < handle
+    %FREQUENCYBAND Provides important information about the frequency band
+    
+    properties
+        name string
+        min {mustBeNumeric}
+        max {mustBeNumeric}
+    end
+
+    methods
+        function obj = FrequencyBand(name,min,max)
+            %FREQUENCYBAND Construct an instance of this class
+            %   set the properties of this class
+            obj.name = name;
+            obj.min = min;
+            obj.max = max;
+        end
+    end
+    
+    methods (Static)
+        function allBands = getAllBands()
+            %GETFREQUENCYBANDS Returns all important frequency bands for
+            % eeg signal processing
+            allBands = [
+                FrequencyBand('delta',0.1,3.5),...
+                FrequencyBand('theta',4,7.5),...
+                FrequencyBand('alpha',8,12.5),...
+                FrequencyBand('beta',13,29.5),...
+                FrequencyBand('gamma',30,60),...
+                FrequencyBand('high-gamma',60.5,100),...
+                FrequencyBand('broad',0.1,100)];
+        end
+
+        function selectedBands = getSpecificBands(bandNames)
+            %GETSPECIFICBANDS Returns the frequency band objects selected 
+            % by the function argument
+            allBands=FrequencyBand.getAllBands;
+            selectedBands=FrequencyBand.empty(0,length(bandNames));
+            for i=1:length(bandNames)
+                 selectedBands(i)=findobj(allBands,'name',bandNames(i));
+            end
+        end
+    end
+end
--- src/extractors/psd_extractor.m
+++ src/extractors/psd_extractor.m
@@ -1,25 +1,24 @@
 function [psd] = psd_extractor(eeg,fs)
 % Calculate and returns the welch's power spectral density estimate
-
-% delta theta alpha beta gamma high gamma broad
-bands=[0.1 3.5; 4 7.5; 8 12.5; 13 29.5; 30 60; 60.5 100; 0.1 100];
+selected_bands=["delta","theta","alpha","beta","gamma","high-gamma","broad"];
+bands=FrequencyBand.getSpecificBands(selected_bands);
 features=["mean","median","std"];
-[channels,N,trials]=size(eeg);
+[channels,~,trials]=size(eeg);
 %nfft=N;
 %pxx_length=inline_if(mod(nfft,2)==0,(nfft/2+1),(nfft+1)/2);
 % TODO: check different windowing methods
 % window=hamming(nfft);
 % window=rectwin(nfft);
-psd=nan(channels*length(features),size(bands,1),trials);
+psd=nan(channels*length(features),length(bands),trials);
 for trial=1:trials
-        x=squeeze(eeg(:,:,trial))';
-        [pxx,f]=pwelch(x,[],[],[],fs);
-        for j=1:size(bands,1)
-            [mm,ii1]=min(abs(f-bands(j,1)));
-            [mm,ii2]=min(abs(f-bands(j,2)));
-                    %psd(:,j,trial)=mean(pxx(ii1:ii2,:));
-                    psd(:,j,trial)=statistic_extractor_2d(pxx(ii1:ii2,:),features);
-        end
+    x=squeeze(eeg(:,:,trial))';
+    [pxx,f]=pwelch(x,[],[],[],fs);
+    for j=1:length(bands)
+        [~,ii1]=min(abs(f-bands(j).min));
+        [~,ii2]=min(abs(f-bands(j).max));
+        psd(:,j,trial)=statistic_extractor_2d(pxx(ii1:ii2,:),features);
+    end
+end
 end
 
 

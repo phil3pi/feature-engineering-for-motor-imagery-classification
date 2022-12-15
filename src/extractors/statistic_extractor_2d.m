1924f592d9a061c436be6de6c548ccaff08561e4
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2022-12-15 17:00:01 +0100

    ST021 implemented slope calculation

--- src/extractors/statistic_extractor_2d.m
+++ src/extractors/statistic_extractor_2d.m
@@ -5,7 +5,7 @@ function [statistic_features] = statistic_extractor_2d(data,selected_features,fs
 %   median, standard-deviation, kurtosis, skewness, percentile, entropy
 [min_data,max_data,mean_data,median_data,...
     std_data,kurtosis_data,skewness_data,prctile_data,entropy_data,...
-    spectral_entropy_data] = deal([]);
+    spectral_entropy_data,slope_data] = deal([]);
 for i=1:length(selected_features)
     if any(strcmp(selected_features,"min"))
         min_data = min(data,[],1);
@@ -32,11 +32,15 @@ for i=1:length(selected_features)
     elseif any(strcmp(selected_features, "spectral-entropy"))
         % TODO: think about the median of entropy values ...
         spectral_entropy_data = median(pentropy(data(:),fs));
+    elseif any(strcmp(selected_features, "slope"))
+        data_points = length(data);
+        assert(data_points > 1, "In order to calculate the slop at least 2 data-points are needed.");
+        slope_data = (data(data_points)-data(1)) / (data_points - 1);
     end
     selected_features(i) = nan;
 end
 statistic_features=[min_data,max_data,mean_data,median_data,...
     std_data,kurtosis_data,skewness_data,prctile_data,entropy_data,...
-    spectral_entropy_data];
+    spectral_entropy_data,slope_data];
 end
 

0ad95bdd38d1ed6e62d9d99781c054e887241441
Philipp Steinwender <philipp.steinwender@student.tugraz.at>
2023-02-15 18:03:37 +0100

    improved feature_main script

--- src/feature_main.m
+++ src/feature_main.m
@@ -13,37 +13,35 @@ setup_multithreading(8);
 
 data = Dataset(1,true);
 data.removeArtifacts();
-%data.removeOutliers("quartiles","spline");
 
 sampling_rates = [250, 50, 25];
 window_sizes = [100, 20, 10];
 
+parameterPermutationList = {StatisticParameters.getPermutations,...
+    PsdParameters.getPermutations, WaveletEntropyParameters.getPermutations,...
+    [WaveletVarianceParameters()], [WaveletCorrelationParameters()],...
+    ArParameters.getPermutations, ArPsdParameters.getPermutations,...
+    [LyapunovParameters()]};
+
 for i = 1:length(sampling_rates)
     data.resample(sampling_rates(i));
-    %parametersList = StatisticParameters.getPermutations;
-    %parametersList = PsdParameters.getPermutations;
-    %parametersList = WaveletEntropyParameters.getPermutations;
-    %parametersList = [WaveletVarianceParameters()];
-    %parametersList = [WaveletCorrelationParameters()];
-    parametersList = ArParameters.getPermutations;
-    %parametersList = ArPsdParameters.getPermutations;
-    %parametersList = [LyapunovParameters()];
-    for parameter = parametersList
-
-        try
-            [accuracy, accuracy_chance, kappa, kappa_chance] = train_classifier(data, window_sizes(i), parameter);
-
-            filename = sprintf('%shz-%s-%s.fig', string(sampling_rates(i)), string(window_sizes(i)), parameter.toString);
-            print_measures(data.N, data.fs, window_sizes(i), accuracy, accuracy_chance, kappa, kappa_chance, filename);
-        catch ME
-            filename = sprintf('0-%shz-%s-%s.txt', string(sampling_rates(i)), string(window_sizes(i)), parameter.toString);
-            fileID = fopen(filename, 'w');
-            fprintf(fileID, "%s\n", ME.identifier);
-            fprintf(fileID, ME.message);
-            disp(ME.message);
-            fclose(fileID);
+    
+    for parameterPermutation = parameterPermutationList
+        for parameter = parameterPermutation{1}
+            try
+                [accuracy, accuracy_chance, kappa, kappa_chance] = train_classifier(data, window_sizes(i), parameter);
+    
+                filename = sprintf('%shz-%s-%s.fig', string(sampling_rates(i)), string(window_sizes(i)), parameter.toString);
+                print_measures(data.N, data.fs, window_sizes(i), accuracy, accuracy_chance, kappa, kappa_chance, filename);
+            catch ME
+                filename = sprintf('0-%shz-%s-%s.txt', string(sampling_rates(i)), string(window_sizes(i)), parameter.toString);
+                fileID = fopen(filename, 'w');
+                fprintf(fileID, "%s\n", ME.identifier);
+                fprintf(fileID, ME.message);
+                disp(ME.message);
+                fclose(fileID);
+            end
+    
         end
-
     end
-
 end
